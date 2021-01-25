41525ad7a93b040b6777bb3f138527d1ee894714
zael-mab <zackliwa@gmail.com>
2021-01-25 18:36:52 +0100

    X|

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,66 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+
+// 1 . i = 32 * row + col/16 => address
+//      world = Screen[ 32*row + col/16 ]
+//      world = RAM [ 16384 + 32*row + col/16 ]
+// 2 . set the  (col%16)th   bit of word to 0 or 1
+// 3 . Commit word to the RAM
+/////////////////////////////////
+// addr = SCREEN
+// n = RAM[0]
+// i = 0
+
+// LOOP:
+//      if  i > n  goto END
+//      RAM[addr] = -1
+    // advanced to the next row
+//     addr = addr + 32
+//     i = i + 1
+//     goto LOOP
+
+// END:
+//     goto END
+//////////////////////////////////
+
+
+    @SCREEN
+    D=A
+    @addr
+    M=D     //  addr = 16384 (screen's base address)
+
+    @0
+    D=M
+    @n
+    M=D     //  n = Ram[0]
+
+    @i
+    M=0     //  i = 0
+
+(LOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @END
+    D;JGT   // (D == 0) if i > n goto END
+
+    @addr
+    A=M
+    M=-1    //   RAM[addr] =1111111111111111
+    
+    @i
+    M=M+1   //  i = i + 1
+    @32
+    D=A
+    @addr
+    M=D+M   //  addr = addr + 32
+    @LOOP
+    0;JMP   //  goto LOOP
+
+(END)
+    @END    //  program's end
+    0;JMP   //  infinite loop
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,6 +6,48 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
+
+    @R0
+    M=D
+
+    @R1
+    M=D
+
+    @R2
+    M=D
+
+(LOOP)
+    @
+    M=0
+    
+
+
+
+    if r1 > r0
+        t0 = r0 = 5 
+        t1 = r1 = 20
+    else
+        t0 = r1
+        t1 = r0
+    x = 1
+    while (x <= 5)
+    {
+        if (x == 1 && (x & 5))
+            sum += 20;
+        if (x & 5)
+        {
+            20 <= 20 << 1
+            sum += t0
+        }
+        x = x + x
+    }
+
+
+
+
+
+
+
 ///////////////////////////
 //Desigb the program using pseudo code
 //-Write the program in assembly language
@@ -16,11 +58,6 @@
 // i   =  |1| |2| |3|
 // sum =  |1| |3| |6|
 
-// 1 . i = 32 * row + col/16 => address
-//      world = Screen[ 32*row + col/16 ]
-//      world = RAM [ 16384 + 32*row + col/16 ]
-// 2 . set the  (col%16)th   bit of word to 0 or 1
-// 3 . Commit word to the RAM
 
 // r0[0..15] , r1[0..15], r2[0..15]=false, t[0..15]=false;
 
@@ -40,43 +77,43 @@
 
 
             // arr = 100
-    @100
-    D=A
-    @arr
-    M=D
+//     @100
+//     D=A
+//     @arr
+//     M=D
 
-    @10    //n = 10
-    D=A
-    @n
-    M=D
+//     @10    //n = 10
+//     D=A
+//     @n
+//     M=D
 
-        // init i = 0
-    @i
-    M=0
+//         // init i = 0
+//     @i
+//     M=0
 
-(LOOP)
-        // if i == n goto END
-    @i
-    D=M
-    @n
-    D=D-M
-    @END
-    D;JEQ
+// (LOOP)
+//         // if i == n goto END
+//     @i
+//     D=M
+//     @n
+//     D=D-M
+//     @END
+//     D;JEQ   //if D == 0 goto End
 
-        // RAM[arr + i] = -1
-    @arr
-    D=M
-    @i
-    A=D+M
-    M=-1
+//         // RAM[arr + i] = -1
+//     @arr
+//     D=M
+//     @i
+//     A=D+M
+//     M=-1
 
-        // i++
-    @i
-    M=M+1
+//         // i++
+//     @i
+//     M=M+1
 
-    @LOOP
-    0;JMP
+//     @LOOP
+//     0;JMP
 
-(END)
-    @END
-    0;JMP
+// (END)
+//     @END
+//     0;JMP
